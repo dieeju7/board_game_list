1c4debc4f4accc9fcdaaf27c09dacdaf7bd5b8b5
Monxter115 <151973590+Monxter115@users.noreply.github.com>
2023-11-25 22:35:14 +0200

    Delete GamerWebsite/GamerWebsite directory

--- GamerWebsite/GamerWebsite/gamers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import BoardGame, BoardGamer, GameLoan
-
-admin.site.register(BoardGame)
-admin.site.register(BoardGamer)
-admin.site.register(GameLoan)
--- GamerWebsite/GamerWebsite/gamers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GamersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gamers'
